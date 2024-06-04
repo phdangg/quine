79ef40333429726b7510e7fe034a1f2a0caa088c
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-06-04 16:25:18 +0700

    implementation of Person class

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Person {
+import java.util.Objects;
+
+public final class Person {
+    private final String name;
+    private final long info;
+
+    public Person(){
+        this.name = "Default";
+        this.info = 1;
+    }
+
+    public Person(String name, long info) {
+        this.name = name;
+        this.info = info;
+    }
+
+    // how you're supposed to implement equals
+    public boolean equals(Object other) {
+        if (other == this) return true;
+        if (other == null) return false;
+        if (other.getClass() != this.getClass()) return false;
+        Person that = (Person) other;
+        return (this.name.equals(that.name)) && (this.info == that.info);
+    }
+
+    public String toString() {
+        return name + " " + info;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(info, name);
+    }
+
+
 }
