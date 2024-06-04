ca22a8faeaace0c3de4f2405ac59957f10d3d374
Laptop - phdangg <dangmanhphuc2003@gmail.com>
2024-06-04 16:24:05 +0700

    Example class for testing

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Example {
+public class Example {
+    private int number;
+    private String text;
+
+    public Example(int number, String text) {
+        this.number = number;
+        this.text = text;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Quine.sourceCode(Example.class));
+    }
 }
